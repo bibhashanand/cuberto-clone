de3fda3f16d0f009aaacaec3b69e5807c7ccd0a1
Bibhash Anand <bibhashanand@outlook.com>
2024-01-23 04:48:00 +0530

    done but an issue

---
+++ script.js
@@ -0,0 +1,34 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle("#homemain",{videos: ["./hero.mp4"]});
+
+gsap.to(".fleftelem",{
+    scrollTrigger: {
+        trigger: "#fimages",
+        pin: true,
+        start: "top top",
+        end: "bottom bottom",
+        endTrigger: ".last",
+        scrub: 1
+    },
+    y: "-300%",
+    ease: Power1
+});
+
+let sections = document.querySelectorAll(".fleftelem");
+Shery.imageEffect(".images", {
+    style: 5,
+    config: {onMouse: {value:1}},
+    slideStyle: (setScroll) => {
+        sections.forEach(function(section, index){
+            ScrollTrigger.create({
+                trigger: section,
+                start: "top top",
+                scrub: 1,
+                onUpdate: function(prog){
+                    setScroll(prog.progress+index);
+                },
+            });
+        });
+    },
+  });
